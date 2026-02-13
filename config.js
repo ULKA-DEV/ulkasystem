ed1f4136fa0a1514322837ff0fdb0286ffd1d6d1
ULKA-DEV <info@ulka.co.th>
2026-02-13 09:59:26 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js - "หัวจ่ายน้ำมัน" ของระบบ ULKA
+const GLOBAL_CONFIG = {
+    // ก๊อป URL ล่าสุดของ Apps Script มาวางที่นี่ที่เดียวพอ!
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxjuIt0g4Hlsh6q-pMIY04eohi94lRkgSoka5k7Ye2RjQNeabgHn5g99_cpCZSxQHjC-Q/exec",
+    
+    BRAND_NAME: "ULKA Enterprise",
+    TARGET_SALES: 7000000,
+    REFRESH_INTERVAL: 60000 // 1 นาที
+};
